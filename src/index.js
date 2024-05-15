386757cc4f7bf5f3c97027355d3b5eabbf413b03
Ritik <ritikraw@gmail.com>
2024-05-15 09:56:12 +0530

    Add global sass style sheet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/App.jsx";
 import { createRoot } from "react-dom/client";
+import "./style.scss";
 
 const root = document.getElementById("root");
 
