c6e6694034c55020b34ac684768709b49963bdd6
Ritik <ritikraw@gmail.com>
2024-05-15 09:58:44 +0530

    Add editor component

---
+++ src/component/Editor/index.jsx
@@ -0,0 +1,36 @@
+import React, { useState, forwardRef } from "react";
+import styles from "./editor.module.scss";
+
+const Editor = forwardRef(({ onSubmit, currentUser, cta = "Send" }, ref) => {
+  const [value, setValue] = useState();
+
+  function onChange(e) {
+    setValue(e.target.value);
+  }
+
+  const Avatar = currentUser?.image.png?.substring(1);
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.user_avatar} srcSet={Avatar} alt="" />
+      <textarea
+        onChange={onChange}
+        value={value}
+        name="comment"
+        id="comment-input"
+        ref={ref}
+      ></textarea>
+      <button
+        className={styles.send}
+        onClick={() => {
+          onSubmit(value);
+          setValue("");
+        }}
+      >
+        {cta}
+      </button>
+    </div>
+  );
+});
+
+export default Editor;
